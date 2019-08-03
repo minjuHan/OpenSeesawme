4c2696469eabc6f2b85025d13afa39d0c108df59
이시원 <lsc00l>
2019-08-04 01:15:29 +0900

    0803 siwon 상세보기까지 완료

--- app/src/main/java/com/example/openseesawme/OtherMemo1.java
+++ app/src/main/java/com/example/openseesawme/OtherMemo1.java
@@ -14,33 +14,80 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class OtherMemo1 extends AppCompatActivity {
-    TextView tvday, tvto, tvfrom;
+    TextView tvday, tvto, tvfrom, txt_guest_date, txt_guest_allow;
     Toolbar myToolbar;
     Button backkey1;
+
+    String[] gData0;    //인덱스
+    String[] gData1;    //출입가능 날짜
+    String[] gData2;    //게스트 이름
+    String[] gData3;    //게스트키 준 날짜
+    String[] gData4;    //게스트키 사용 여부
+    String[] gData5;    //게스트키 수락 여부
+    String[] otherJun;  //게스트키 준 사람 이름
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_othermemo);
         setTitle("게스트키 상세 정보");
+        txt_guest_allow = findViewById(R.id.txt_guest_allow);
+        txt_guest_date = findViewById(R.id.txt_guest_date);
 
-        String strday = "매주 토요일";
+        //인텐트 받기(OtherGuestkey.java 로 부터 받은 값)
+        Intent intent = getIntent();
+        int position = (Integer)intent.getExtras().get("position");
+        String result = intent.getStringExtra("result");
+
+        String[] receiveData;
+        receiveData = result.split("_spl_");
+
+        String[] otherG;
+        otherG = receiveData[0].split("\t");
+        gData0 = otherG[0].split(" ");  //d_guest_index
+        gData1 = otherG[1].split(" ");  //게스트 출입 날짜
+        gData2 = otherG[2].split(" ");  //게스트 이름
+        gData3 = otherG[3].split(" ");  //게스트키 준 날짜
+        gData4 = otherG[4].split(" ");  //사용여부
+        gData5 = otherG[5].split(" ");  //수락 여부
+
+        otherJun = receiveData[1].split(" ");   //게스트키 준 사람 이름
+
+
+        String strday = gData1[position];       //"매주 토요일"
         TextView tvday = findViewById(R.id.tvday);
         SpannableStringBuilder day = new SpannableStringBuilder(strday);
         day.setSpan(new ForegroundColorSpan(Color.parseColor("#03A9F4")), 3, 6, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvday.setText(day);
 
-        String strto = "To. 이혜원 님";
+        String strto = "To. " + gData2[position] + " 님";
         TextView tvto = findViewById(R.id.tvto);
         SpannableStringBuilder to = new SpannableStringBuilder(strto);
         to.setSpan(new ForegroundColorSpan(Color.parseColor("#444444")), 0, 7, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvto.setText(to);
 
-        String strfrom = "From. 이시원 님";
+        String strfrom = "From. "+ otherJun[position] +" 님";
         TextView tvfrom = findViewById(R.id.tvfrom);
         SpannableStringBuilder from = new SpannableStringBuilder(strfrom);
         from.setSpan(new ForegroundColorSpan(Color.parseColor("#444444")), 0, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvfrom.setText(from);
+
+        txt_guest_date.setText(gData3[position] + "에 전송됨");
+
+        if(gData4[position].equals("a") && gData5[position].equals("a")){
+            txt_guest_allow.setText("게스트키 상태 : 미수락");
+        }
+        else if(gData4[position].equals("a") && gData5[position].equals("b")){
+            txt_guest_allow.setText("게스트키 상태 : 미사용");
+        }
+        else if(gData4[position].equals("b") && gData5[position].equals("b")){
+            txt_guest_allow.setText("게스트키 상태 : 사용 완료");
+        }else{
+            txt_guest_allow.setText("값을 잘못 넣었다.!");
+        }
+
+
+
 
         // 추가된 소스, Toolbar를 생성한다.
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
@@ -54,13 +101,12 @@ public class OtherMemo1 extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         //여기까지 툴바
 
-        //되돌아가기
+
+        //목록으로 버튼
         backkey1 = findViewById(R.id.backkey1);
         backkey1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(),OtherGuestkey.class);
-//                startActivity(intent);
                 finish();
             }
         });
